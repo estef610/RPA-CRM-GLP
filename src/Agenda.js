d95f3b65e7685dfd94ea3ec8f60364c6b93fd62c
estef610 <70669723+estef610@users.noreply.github.com>
2020-09-04 21:31:28 -0500

    primer commit

---
+++ src/Agenda.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Agenda(props){
+    return(
+        <h1>Agendados</h1>
+    );
+}
+
+export default Agenda;
---
+++ src/Perfil.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Perfil(props){
+    return(
+        <h1>Unidad</h1>
+    );
+}
+
+export default Perfil;
