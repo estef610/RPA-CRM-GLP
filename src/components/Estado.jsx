6afebedbc588729071fef2b003da412b5e15fbdc
estef610 <70669723+estef610@users.noreply.github.com>
2020-09-06 00:29:17 -0500

    quinto commit se agrego estado y nuevo u

---
+++ src/components/Estado.jsx
@@ -0,0 +1,22 @@
+import React, {useState}  from 'react';
+
+
+const Estado= ()=> {
+
+    const [texto, setTexto]= useState('Activa')
+
+    const estadoClick= ()=> {
+        setTexto('Inactiva')
+    }
+
+
+    return (
+        <div>
+           <h3>{texto}</h3> 
+           <button onClick={ ()=> estadoClick()}>Unidad</button>
+        </div>
+        
+    )
+}
+
+export default Estado
---
+++ src/components/Nuevau.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Nuevau= ()=> {
+
+    const [Unidad,setUnidad]= React.useState('')
+
+    return (
+        <div>
+                <br/>
+            <h3>Registro de Nueva Unidad</h3>
+                <br/>
+            <h5>Ingrese ...</h5>
+                <br/>
+
+            <div>
+                <form action="">
+                    <input type="text" className="form-control mb-2" placeholder="Tipo Unidad"/>
+                    <input type="text" className="form-control mb-2" placeholder="Cabina" />
+                    <input type="text" className="form-control mb-2" placeholder="Id Unidad" />
+                    <input type="text" className="form-control mb-2" placeholder="Id Operador" />
+                    <input type="text" className="form-control mb-2" placeholder="Asigne el Horario" />
+                    <input type="text" className="form-control mb-2" placeholder="Ingrese las colonias de Cobertura" />
+                    <input type="text" className="form-control mb-2" placeholder="Ingrese las colonias de BlackList" />
+                    <input type="text" className="form-control mb-2" placeholder="Espesifique días de actividad" />
+            
+                    <button onClick="" >Agregar</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Nuevau
