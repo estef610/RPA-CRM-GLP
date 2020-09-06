ea2e69541f92d6b4b478130ee0b6f922e92e0b70
estef610 <70669723+estef610@users.noreply.github.com>
2020-09-06 00:16:53 -0500

    cuarto commit

--- src/Perfil.js
+++ src/Perfil.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import Search from './Search.js';
+import Estado from "./components/Estado.jsx";
+import Nuevau from './components/Nuevau.jsx';
 
 function Perfil(props){
     return(
-        <h1>Unidad</h1>
+
+        <div className="container">
+            <div className="subcontainer">
+
+            { /*   <div className="logo">
+                    {props.title}
+                </div> */}
+
+                <div className="search">
+                    <Search />
+                </div>
+ 
+
+                <div className="Estado">
+                    <Estado/>
+                </div> 
+
+                <div className="NuevaU">
+                    <Nuevau/>
+                    <button className='button new-u' onClick="./components/Nuevau.jsx">Nueva Unidad</button>
+                </div> 
+
+            </div>
+        </div>
     );
 }
 
